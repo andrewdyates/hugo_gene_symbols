bc6ee1d7e72bb8477fd64c693c9bf323bfbbf8e3
Andrew Yates <andrewyates.name@gmail.com>
2013-06-07 20:56:43 -0400

    export sym list

--- __init__.py
+++ __init__.py
@@ -4,6 +4,10 @@
 USE:
 >>> import hugo_gene_symbols
 >>> H = hugo_gene_symbols.load()
+>>> print H.find_sym("MySym") # only unique assignments
+>>> print H.find_sym("MySym", allow_dupe=True) # may return a set of symbols
+>>> print H.official["MySym"] # dict of all known attributes about official symbol
+>>> print H.entrez["12345"] # official symbol that corresponds to entrez ID, if any
 """
 import cPickle as pickle
 import os.path
--- parse_hugo.py
+++ parse_hugo.py
@@ -13,9 +13,15 @@ from hugo_gene_symbols.hugo import Hugo, FNAME, FNAME_PKL
 def main():
   print "Loading %s into Hugo object..." % FNAME
   H = Hugo()
+  H.from_filename = FNAME
   print "Saving pickled Hugo object to %s..." % FNAME_PKL
   pickle.dump(H, open(FNAME_PKL,"w"), -1)
-
+  idx_fname = FNAME_PKL.rpartition('.')[0]+".idx.txt"
+  print "Saving index as plain text to %s..." % idx_fname
+  fp = open(idx_fname,"w")
+  for s in sorted(H.official.keys()):
+    fp.write("%s\n"%s)
+  fp.close()
 
 if __name__ == "__main__":
   main()
